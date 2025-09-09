75f38b067ab20b78220a4783a0688faf62dd20cd
BurningStar0100 <b21157@students.iitmandi.ac.in>
2025-09-09 21:52:31 +0530

    Added query answering

--- rag.py
+++ rag.py
@@ -1,6 +1,23 @@
-# Advanced RAG components
-from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import DocumentCompressorPipeline
-from langchain.retrievers.document_compressors import FlashrankRerank
-from langchain.retrievers.document_compressors import EmbeddingsFilter
+"""Implement RAG pipeline:- takes user query, fetch relevant document, use llm to generate response and return it to user with citiations"""
+
+from main import llm, vector_store, embeddings
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain import hub
+
+def generate_response(user_query:str):
+    #getting relevant docs form vector store
+    results = vector_store.similarity_search_by_vector(
+    embedding=embeddings.embed_query(user_query), k=3
+    )
+    for doc in results:
+        print(f"* {doc.page_content} [{doc.metadata}]")
+    # Define prompt for question-answering
+    prompt = hub.pull("rlm/rag-prompt")
+    docs_content = "\n\n".join(doc.page_content for doc in results)
+    messages = prompt.invoke({"question": user_query, "context": docs_content})
+    response = llm.invoke(messages)
+    print(f"Response: {response.content}")
+    return {"answer": response.content + "\n\n" + "SOURCES:\n" + "\n".join(set(doc.metadata["source"] for doc in results))}
+    
+    
--- routes/query.py
+++ routes/query.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
+from rag import generate_response
+
 
 query_router = APIRouter()
 
 @query_router.post("/{user_query}")
 def handle_query(user_query: str):
     """Handle user queries."""
-    return {"query": user_query, "response": "This is a placeholder response."}
+    return generate_response(user_query)
